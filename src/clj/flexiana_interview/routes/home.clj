88964c32578f8d90295a8406e82a6095ef2fa9a1
zsolt kormany <zsoltkormany@gmail.com>
2021-07-25 12:29:14 +0200

    add endpoint for checking scrambles

--- src/clj/flexiana_interview/routes/home.clj
+++ src/clj/flexiana_interview/routes/home.clj
@@ -4,17 +4,26 @@
    [clojure.java.io :as io]
    [flexiana-interview.middleware :as middleware]
    [ring.util.response]
-   [ring.util.http-response :as response]))
+   [ring.util.http-response :as response]
+   [flexiana-interview.logic :refer [scramble?]]))
 
 (defn home-page [request]
   (layout/render request "home.html"))
+
+(defn check-scramble [request]
+  (let [params (:query-params request)
+        charlist (get params "charlist")
+        word (get params "word")]
+    (if (not (or (empty? charlist) (empty? word)))
+      (response/ok {:body {:params params :answer (scramble? charlist word)}})
+      (response/ok {:body {:error "Wrong parameters"}}))))
 
 (defn home-routes []
   [""
    {:middleware [middleware/wrap-csrf
                  middleware/wrap-formats]}
    ["/" {:get home-page}]
+   ["/check-scramble" {:get check-scramble}]
    ["/docs" {:get (fn [_]
                     (-> (response/ok (-> "docs/docs.md" io/resource slurp))
                         (response/header "Content-Type" "text/plain; charset=utf-8")))}]])
-
